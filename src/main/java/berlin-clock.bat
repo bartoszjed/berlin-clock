82ee46ea60b9d0ed55b27829d8ec882a9405d291
Bartosz Jedrzejczak <bartoszjed@gmail.com>
2016-12-20 22:33:04 +0000

    Circle light added Batch execution file added

---
+++ src/main/java/berlin-clock.bat
@@ -0,0 +1,1 @@
+java org.bj.BerlinClock.App
--- src/main/java/org/bj/BerlinClock/ClockRenderer.java
+++ src/main/java/org/bj/BerlinClock/ClockRenderer.java
@@ -19,6 +19,8 @@ class ClockRenderer{
 
 
     ClockRenderer (String[] berlinerTime){
+        System.out.println(circleLight(berlinerTime[4]));
+
         System.out.println(rowOfLights(berlinerTime[0]));
         System.out.println(rowOfLights(berlinerTime[1]));
         System.out.println(rowOfLights(berlinerTime[2]));
@@ -28,12 +30,12 @@ class ClockRenderer{
 
     private String rowOfLights(String values){
         int buttonCount = values.length();
-        Boolean big = (buttonCount==4?true:false);
+        Boolean big = (buttonCount==4);
 
         String bigHorizontalBorder = stringFiller(6, HORIZONTAL_BORDER);
         String bigSpace = stringFiller(bigHorizontalBorder.length()/2, ' ');
 
-        String result = (big?"":" ");;
+        String result = (big?"":" ");
         for(int i=0; i< buttonCount;i++){
             result += String.valueOf(TOP_LEFT_CORNER) + HORIZONTAL_BORDER + (big?bigHorizontalBorder :"") + TOP_RIGHT_CORNER;
         }
@@ -42,7 +44,7 @@ class ClockRenderer{
         result += (big?"":" ");
         for(int i=0; i< buttonCount;i++){
             String currentVal = values.substring(i,i+1);
-            result += VERTICAL_BORDER + (big?bigSpace:"") + currentVal + (big?bigSpace:"") + VERTICAL_BORDER;;
+            result += VERTICAL_BORDER + (big?bigSpace:"") + currentVal + (big?bigSpace:"") + VERTICAL_BORDER;
         }
         result += NEW_LINE;
 
@@ -55,11 +57,35 @@ class ClockRenderer{
         return result;
     }
 
+    private String circleLight(String value){
+
+        int halfSize = 18;
+        String left1 = stringFiller(halfSize,"","*",' ');
+        String left2 = stringFiller(halfSize,"","*  ",' ');
+        String middle = stringFiller(halfSize,"","*    ",' ');
+
+        String result = left1 + " " + stringReverse(left1) + NEW_LINE;
+        result += left2 + " " +stringReverse(left2) + NEW_LINE;
+        result += middle + value +stringReverse(middle) + NEW_LINE;
+        result += left2 + " " +stringReverse(left2) + NEW_LINE;
+        result += left1 + " " +stringReverse(left1) + NEW_LINE;
+
+        return result;
+    }
 
     private String stringFiller(int size, char filler) {
-        char[] chars = new char[size];
+        return stringFiller(size,"","",filler);
+    }
+
+    private String stringFiller(int size, String startsWith, String endsWith, char filler) {
+        char[] chars = new char[size-startsWith.length()-endsWith.length()];
         Arrays.fill(chars, filler);
-        return new String(chars);
+        return startsWith.concat(new String(chars)).concat(endsWith);
+    }
+
+    private String stringReverse(String in){
+        return (new StringBuilder()).append(in).reverse().toString();
+
     }
 
 }
